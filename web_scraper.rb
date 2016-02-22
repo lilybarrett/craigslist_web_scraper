fa3496f86cf1c4a1d5eb0084ac8976d436aac8c0
lilybarrett <lily@barrett.us>
2016-02-22 13:48:45 -0500

    convert to a sinatra app

--- web_scraper.rb
+++ web_scraper.rb
@@ -1,24 +1,27 @@
+require 'sinatra'
 require 'HTTParty'
 require 'Nokogiri'
 require 'JSON'
-require 'Pry'
+require 'pry'
 require 'csv'
 
-page = HTTParty.get('https://newyork.craigslist.org/search/pet?s=0')
+set :views, File.join(File.dirname(__FILE__), "app/views")
 
-parse_page = Nokogiri::HTML(page)
+before do
+  page = HTTParty.get('https://newyork.craigslist.org/search/pet?s=0')
+  parse_page = Nokogiri::HTML(page)
+  @pets_array = []
 
-pets_array = []
+  parse_page.css('.content').css('.row').css('.hdrlnk').map do |a|
+    post_name = a.text
+    @pets_array.push(post_name)
+  end
 
-parse_page.css('.content').css('.row').css('.hdrlnk').map do |a|
-  post_name = a.text
-  pets_array.push(post_name)
+  CSV.open('pets.csv', 'w') do |csv|
+    csv << @pets_array
+  end
 end
 
-CSV.open('pets.csv', 'w') do |csv|
-  csv << pets_array
-end
-
-pets_array.each do |title|
-  puts title 
+get '/' do
+  erb :index
 end
